50166954f4e1cd25541b1eda4df9408fa4c9bfe6
wendy <mizukilee@gmail.com>
2023-02-27 18:40:03 -0500

    commit tree 0e0ccb05db087862614c2361e659f95185de6873

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items 
+    else 
+      items = Item.all 
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+
+    if item.persisted?
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end 
+
+  private 
+
+  def not_found_response
+    render json: { error: "Not found" }, status: :not_found
   end
 
 end
